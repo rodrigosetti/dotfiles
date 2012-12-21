9547ab302356b8fce9d94d3b55c1d180ebcf9778
Rodrigo Setti <rodrigosetti@gmail.com>
2012-12-21 11:27:28 -0800

    Bugfixes in install script; .vim improvements
    Install has a special treatment for the .vim directory. Now the backup
    and tmp subdirectories are created locally (instead of being linked to
    the repo).
    Added more color schemes to .vim/colors: jellybeans, candy and distinguished.
    Bugfix in installation of the .config directory.

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
+# For every dot_* file in installation
 for REP_FILE in `ls -d dot_*`; do
     DOT_FILE=`echo "$REP_FILE" | sed 's/^dot_/./'`
 
+    # If file in home directory is a link, then we remove it
     if [ -L "$HOME/$DOT_FILE" ]; then
         rm -v "$HOME/$DOT_FILE"
     fi
 
+    # If file in home does not exists as file or directory, create the link
     if [ ! -f "$HOME/$DOT_FILE" -a ! -d "$HOME/$DOT_FILE" ]; then
         ln -vs "`pwd`/$REP_FILE" "$HOME/$DOT_FILE"
     else
@@ -15,19 +18,48 @@ for REP_FILE in `ls -d dot_*`; do
 done
 
 # Creates .config directory in home, if not exists
-[ ! -d "$HOME/.config" ] && mkdir -v .config
+[ ! -d "$HOME/.config" ] && mkdir -v "$HOME/.config"
 
-for REP_DIR in `ls config`; do
-    CONFIG_DIR=".config/$REP_DIR"
+# For every file in installation config directory
+for FILE_NAME in `ls config`; do
+    HOME_FILEPATH="$HOME/.config/$FILE_NAME"
 
-    if [ -L "$HOME/$CONFIG_DIR" ]; then
-        rm -v "$HOME/$CONFIG_DIR"
+    # If file in home directory is a link, then we remove it
+    if [ -L "$HOME_FILEPATH" ]; then
+        rm -v "$HOME_FILEPATH"
     fi
 
-    if [ ! -f "$HOME/$CONFIG_DIR" -a ! -d "$HOME/$CONFIG_DIR" ]; then
-        ln -vs "`pwd`/config/$REP_DIR" "$HOME/$CONFIG_DIR"
+    # If file in home does not exists as file or directory, create the link
+    if [ ! -f "$HOME_FILEPATH" -a ! -d "$HOME_FILEPATH" ]; then
+        ln -vs "`pwd`/config/$FILE_NAME" "$HOME_FILEPATH"
     else
-        echo "File exists and is not a link, not writing: $HOME/$CONFIG_DIR" > /dev/stderr
+        echo "File exists and is not a link, not writing: $HOME_FILEPATH" > /dev/stderr
+    fi
+done
+
+###### Special installation for .vim directory ######
+
+# Creates .vim directory in home, if not exists
+[ ! -d "$HOME/.vim" ] && mkdir -v "$HOME/.vim"
+
+# Create backup and temporary directories in .vim (to be local)
+[ ! -d "$HOME/.vim/backup" ] && mkdir -v "$HOME/.vim/backup"
+[ ! -d "$HOME/.vim/tmp" ] && mkdir -v "$HOME/.vim/tmp"
+
+# For every file in installation vim directory
+for FILE_NAME in `ls vim`; do
+    HOME_FILEPATH="$HOME/.vim/$FILE_NAME"
+
+    # If file in home directory is a link, then we remove it
+    if [ -L "$HOME_FILEPATH" ]; then
+        rm -v "$HOME_FILEPATH"
+    fi
+
+    # If file in home does not exists as file or directory, create the link
+    if [ ! -f "$HOME_FILEPATH" -a ! -d "$HOME_FILEPATH" ]; then
+        ln -vs "`pwd`/vim/$FILE_NAME" "$HOME_FILEPATH"
+    else
+        echo "File exists and is not a link, not writing: $HOME_FILEPATH" > /dev/stderr
     fi
 done
 
